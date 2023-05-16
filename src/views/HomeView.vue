c19d5dc24593aa128ade40ec2096046a91a63fa4
TongZhang <201850030@smail.nju.edu.cn>
2023-05-16 17:21:58 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <el-button type="primary">我是按钮</el-button>
+    <div>测试:{{ user.userName }}}</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from "axios";
+export default {
+  name: "HomeView",
+  data() {
+    return {
+      user: {},
+    };
+  },
+  created() {
+    axios
+      .post(
+        "/api/user/info",
+        {},
+        {
+          headers: {
+            token: "666666",
+          },
+        }
+      )
+      .then((res) => {
+        if (res.data.code === 0) {
+          this.user = res.data.data;
+        }
+      });
+  },
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  devServer: {
+    // 设置代理
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000", // 访问数据的计算机域名
+        ws: true, // 是否启用websockets
+        changOrigin: true, //开启代理,
+        pathRewrite: {
+          // 重写代理规则，/api开头，代理到/
+          "^/api": "/",
+          // 例：/api/user/login代理到
+          // http://localhost:5000/user/login
+        },
+      },
+    },
+  },
+};
