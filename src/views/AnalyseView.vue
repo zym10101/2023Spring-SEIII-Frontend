a90b2aa4cd2a101b2e2a42ecd0f7fc339eb83151
Yueming <201830115@smail.nju.edu.cn>
2023-05-19 17:21:23 +0800

    消除eslint格式告警，测试爬取issue以及comment

--- src/views/AnalyseView.vue
+++ src/views/AnalyseView.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="analyse">
-    <div>当前状态:{{ message }}</div>
-    <el-button type="primary" id="scrapy_issue" @click="scrapy_issue()"
-      >爬取issues
-    </el-button>
-    <el-button type="primary" id="scrapy_comment" @click="scrapy_comment()"
-      >爬取comments
-    </el-button>
+    <el-button type="primary" id="scrapy_issue" @click="scrapy_issue()">爬取issues</el-button>
+    <el-button type="primary" id="scrapy_comment" @click="scrapy_comment()">爬取comments</el-button>
+
+    <p>当前爬取问题状态:{{ issue_message }}</p>
+    <p>当前爬取评论状态:{{ comment_message }}</p>
+
   </div>
 </template>
 
@@ -18,21 +17,30 @@ export default {
   name: "AnalyseView",
   data() {
     return {
-      message: "",
+      issue_message: "未处理",
+      comment_message: "未处理",
     };
   },
   methods: {
     scrapy_issue() {
-      this.message = "正在爬取问题，请稍候……";
-      axios.get("/api/issue/get-and-save-db").then((res) => {
-        this.message = res.data;
-      });
+      if (this.issue_message === "未处理") {
+        this.issue_message = "正在爬取问题，请稍候……";
+        axios.get("/api/issue/get-and-save-db").then((res) => {
+          this.issue_message = res.data;
+        });
+      } else {
+        alert("问题已爬取，请不要重试");
+      }
     },
     scrapy_comment() {
-      this.message = "正在爬取问题评论，请稍候……";
-      axios.get("/api/issue/comments/get-and-save-db").then((res) => {
-        this.message = res.data;
-      });
+      if (this.comment_message === "未处理") {
+        this.comment_message = "正在爬取问题评论，请稍候……";
+        axios.get("/api/issue/comments/get-and-save-db").then((res) => {
+          this.comment_message = res.data;
+        });
+      } else {
+        alert("问题评论已爬取，请不要重试");
+      }
     },
   },
 };
