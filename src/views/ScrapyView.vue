1d3fdfc724985c85454529a32b63fb0622c5b7e0
Yueming <201830115@smail.nju.edu.cn>
2023-05-25 15:42:00 +0800

    feat:三种格式（csv，xlsx，txt）下载

--- src/views/ScrapyView.vue
+++ src/views/ScrapyView.vue
@@ -41,23 +41,49 @@
       <h2>数据获取</h2>
       <el-form label-width="20%">
         <el-form-item label="爬取问题">
-          <el-button type="primary" id="scrapy_issue" @click="scrapy_issue">开始</el-button>&nbsp;&nbsp;
+          <el-button type="primary" id="scrapy_issue" @click="scrapy_issue">开始</el-button>&nbsp;
+          <el-button type="success" id="scrapy_issue_detail" @click="issueTableVisible = true">查看详情</el-button>&nbsp;&nbsp;
           <el-text>当前状态:{{ issue_message }}</el-text>
         </el-form-item>
         <el-form-item label="爬取评论">
-          <el-button type="primary" id="scrapy_comment" @click="scrapy_comment">开始</el-button>&nbsp;&nbsp;
+          <el-button type="primary" id="scrapy_comment" @click="scrapy_comment">开始</el-button>&nbsp;
+          <el-button type="success" id="scrapy_comment_detail" @click="commentTableVisible = true">查看详情</el-button>&nbsp;&nbsp;
           <el-text>当前状态:{{ comment_message }}</el-text>
         </el-form-item>
       </el-form>
+    </div>
+    <div class="issueTable">
+      <el-dialog v-model="issueTableVisible" title="问题列表">
+        <el-table :data="issueData" id="issueDataTable">
+          <el-table-column property="title" label="Title" width="150"/>
+          <el-table-column property="body" label="Body" width="150"/>
+          <el-table-column property="labels" label="Labels" width="150"/>
+          <el-table-column property="create_at" label="Create_at" width="150"/>
+          <el-table-column property="user" label="User" width="150"/>
+          <el-table-column property="reactions" label="Reactions" width="150"/>
+        </el-table>
+      </el-dialog>
+    </div>
+    <div class="commentTable">
+      <el-dialog v-model="commentTableVisible" title="评论列表">
+        <el-table :data="commentData" id="commentDataTable">
+          <el-table-column property="title" label="Title" width="150"/>
+          <el-table-column property="body" label="Body" width="150"/>
+          <el-table-column property="create_at" label="Create_at" width="150"/>
+          <el-table-column property="user" label="User" width="150"/>
+          <el-table-column property="reactions" label="Reactions" width="150"/>
+        </el-table>
+      </el-dialog>
     </div>
     <div class="step3">
       <h2>数据下载</h2>
       <el-form label-width="20%">
         <el-form-item label="下载格式">
-          <el-radio v-model="radio" label="1">csv</el-radio>
-          <el-radio v-model="radio" label="2">xlsx</el-radio>
-          <el-radio v-model="radio" label="3">txt</el-radio>
-          <el-button type="primary" id="scrapy_comment" @click="download">下载</el-button>
+          <el-radio v-model="radio" label="csv">csv</el-radio>
+          <el-radio v-model="radio" label="xlsx">xlsx</el-radio>
+          <el-radio v-model="radio" label="txt">txt</el-radio>
+          <el-button type="primary" id="download_issue" @click="download('issues',this.radio)">下载问题</el-button>
+          <el-button type="primary" id="download_comment" @click="download('comments',this.radio)">下载评论</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -67,18 +93,37 @@
 <script>
 // @ is an alias to /src
 import axios from "axios";
+import * as Papa from 'papaparse';
+import * as XLSX from 'xlsx';
 
 export default {
   name: "ScrapyView",
   data() {
     return {
+      issueData: [{
+        title: 'a',
+        body: 'b',
+        labels: 'c',
+        create_at: '2023-05-04T20:22:49Z',
+        user: 'd',
+        reactions: 'e'
+      }],
+      commentData: [{
+        title: 'a',
+        body: 'b',
+        create_at: '2023-05-04T20:22:49Z',
+        user: 'd',
+        reactions: 'e'
+      }],
+      issueTableVisible: false,
+      commentTableVisible: false,
       form: {
         name: '',
         version: '',
         startTime: '',
         endTime: '',
       },
-      radio: '1',
+      radio: 'csv',
       issue_message: "未处理",
       comment_message: "未处理",
       repo_name: "未定义",
@@ -165,9 +210,47 @@ export default {
         this.repoNullError()
       }
     },
-    download() {
-      // todo
-      console.log('download!');
+    generateData(type, format) {
+      const data = type === 'issues' ? this.issueData : this.commentData;
+      if (format === 'csv') {
+        const csvData = Papa.unparse(data);
+        return {data: csvData, extension: 'csv'};
+      } else if (format === 'xlsx') {
+        const worksheet = XLSX.utils.json_to_sheet(data);
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, '评论');
+        const xlsxData = XLSX.write(workbook, {type: 'array', bookType: 'xlsx'});
+        return {data: xlsxData, extension: 'xlsx'};
+      } else if (format === 'txt') {
+        let txtData = '';
+        if (type === "issues") {
+          txtData += "Title\tBody\tCreate_at\tUser\tReactions\n";
+          data.forEach(item => {
+            txtData += `${item.title}\t${item.body}\t${item.create_at}\t${item.user}\t${item.reactions}\n`;
+          });
+        } else if (type === "comments") {
+          txtData += "Title\tBody\tLabels\tCreate_at\tUser\tReactions\n";
+          data.forEach(item => {
+            txtData += `${item.title}\t${item.body}\t${item.labels}\t${item.create_at}\t${item.user}\t${item.reactions}\n`;
+          });
+        }
+        return {data: txtData, extension: 'txt'};
+      } else {
+        return null; // 处理未知格式
+      }
+    },
+    download(type, format) {
+      const formatData = this.generateData(type, format);
+      if (formatData) {
+        const {data, extension} = formatData;
+        const blob = new Blob([data], {type: `text/${extension};charset=utf-8;`});
+        const downloadLink = document.createElement('a');
+        downloadLink.href = URL.createObjectURL(blob);
+        downloadLink.download = `${type}.${extension}`;
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+      }
     },
   },
 };
