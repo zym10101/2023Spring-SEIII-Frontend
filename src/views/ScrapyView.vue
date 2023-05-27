baae7d3e5427c576145ec155bab20dd1fc8f3ea4
Yueming <201830115@smail.nju.edu.cn>
2023-05-27 18:30:53 +0800

    feat:重构前端

--- src/views/ScrapyView.vue
+++ src/views/ScrapyView.vue
@@ -3,104 +3,97 @@
     <div class="step1">
       <h2>数据来源</h2>
       <el-form ref="form" :model="form" label-width="20%">
-        <el-form-item label="爬取项目链接">
-          <el-input v-model="form.name"></el-input>
+        <el-form-item label="仓库名称" prop="repo"
+                      :rules="[{required: true, message: '请输入仓库名称', trigger: 'blur'}]">
+          <el-input v-model="form.repo"></el-input>
         </el-form-item>
-        <el-form-item label="默认版本时间">
-          <el-select v-model="form.version" placeholder="请选择版本时间">
-            <el-option label="2023.4-2023.6" value="2023.3-2023.6"></el-option>
-            <el-option label="2023.1-2023.3" value="2023.1-2023.3"></el-option>
-            <el-option label="2022.10-2022.12" value="2022.10-2022.12"></el-option>
-            <el-option label="2022.7-2022.9" value="2022.7-2022.9"></el-option>
-            <el-option label="2022.4-2022.6" value="2022.4-2022.6"></el-option>
-            <el-option label="2022.1-2022.3" value="2022.1-2022.3"></el-option>
-            <el-option label="2021.10-2021.12" value="2021.10-2021.12"></el-option>
-            <el-option label="2021.7-2021.9" value="2021.7-2021.9"></el-option>
-            <el-option label="2021.4-2021.6" value="2021.4-2021.6"></el-option>
-            <el-option label="2021.1-2021.3" value="2021.1-2021.3"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="自定义版本时间">
+        <el-form-item label="版本时间">
           <el-col :span="11">
-            <el-date-picker type="date" placeholder="起始时间" v-model="form.startTime"
+            <el-date-picker type="date" placeholder="起始时间" v-model="form.since"
                             style="width: 100%;"></el-date-picker>
           </el-col>
           <el-col class="line" :span="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;至</el-col>
           <el-col :span="11">
-            <el-date-picker type="date" placeholder="结束时间" v-model="form.endTime"
+            <el-date-picker type="date" placeholder="结束时间" v-model="form.until"
                             style="width: 100%;"></el-date-picker>
           </el-col>
         </el-form-item>
+        <el-form-item label="个人邮箱" prop="email" :rules="[
+      { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+      { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }]">
+          <el-input v-model="form.email" autocomplete="off"></el-input>
+        </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">保存</el-button>&nbsp;&nbsp;
+          <el-button type="primary" @click="onSubmit">保存</el-button>&nbsp;
+          <el-button type="primary" @click="crawling">爬取</el-button>&nbsp;&nbsp;
           <el-text>当前项目:{{ repo_name }}</el-text>
         </el-form-item>
       </el-form>
     </div>
-    <div class="step2">
-      <h2>数据获取</h2>
-      <el-form label-width="20%">
-        <el-form-item label="爬取问题">
-          <el-button type="primary" id="scrapy_issue" @click="scrapy_issue">开始</el-button>&nbsp;
-          <el-button type="success" id="scrapy_issue_detail" @click="issueTableVisible = true">查看详情</el-button>&nbsp;&nbsp;
-          <el-text>当前状态:{{ issue_message }}</el-text>
-        </el-form-item>
-        <el-form-item label="爬取评论">
-          <el-button type="primary" id="scrapy_comment" @click="scrapy_comment">开始</el-button>&nbsp;
-          <el-button type="success" id="scrapy_comment_detail" @click="commentTableVisible = true">查看详情</el-button>&nbsp;&nbsp;
-          <el-text>当前状态:{{ comment_message }}</el-text>
-        </el-form-item>
-      </el-form>
-    </div>
-    <div class="issueTable">
-      <el-dialog v-model="issueTableVisible" title="问题列表">
-        <el-table :data="issueData" id="issueDataTable">
-          <el-table-column property="title" label="Title" width="150" show-overflow-tooltip/>
-          <el-table-column property="body" label="Body" width="150" show-overflow-tooltip/>
-          <el-table-column property="labels" label="Labels" width="150" show-overflow-tooltip/>
-          <el-table-column property="created_at" label="Create_at" width="150" show-overflow-tooltip/>
-          <el-table-column property="user" label="User" width="150" show-overflow-tooltip/>
-        </el-table>
-      </el-dialog>
-    </div>
-    <div class="commentTable">
-      <el-dialog v-model="commentTableVisible" title="评论列表">
-        <el-table :data="commentData" id="commentDataTable">
-          <el-table-column property="body" label="Body" width="250" show-overflow-tooltip/>
-          <el-table-column property="created_at" label="Create_at" width="250" show-overflow-tooltip/>
-          <el-table-column property="user" label="User" width="250" show-overflow-tooltip/>
-        </el-table>
-      </el-dialog>
-    </div>
-    <div>
-      <el-dialog title="请填写个人邮箱，以便数据爬取完成后及时通知您~" v-model="emailFormVisible">
-        <el-form :model="emailForm">
-          <el-form-item label="邮箱" prop="email" :label-width="formLabelWidth" :rules="[
-      { required: true, message: '请输入邮箱地址', trigger: 'blur' },
-      { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }]">
-            <el-input v-model="emailForm.email" autocomplete="off"></el-input>
-          </el-form-item>
-        </el-form>
-        <template #footer>
-    <span class="dialog-footer">
-      <el-button @click="emailFormVisible = false">取 消</el-button>
-      <el-button type="primary" @click="submitEmailForm">确 定</el-button>
-    </span>
-        </template>
-      </el-dialog>
-    </div>
-    <div class="step3">
-      <h2>数据下载</h2>
-      <el-form label-width="20%">
-        <el-form-item label="下载格式">
-          <el-radio v-model="radio" label="csv">csv</el-radio>
-          <el-radio v-model="radio" label="xlsx">xlsx</el-radio>
-          <el-radio v-model="radio" label="txt">txt</el-radio>
-          <el-button type="primary" id="download_issue" @click="download('issues',this.radio)">下载问题</el-button>
-          <el-button type="primary" id="download_comment" @click="download('comments',this.radio)">下载评论</el-button>
-        </el-form-item>
-      </el-form>
-    </div>
+<!--    <div class="step2">-->
+<!--      <h2>数据获取</h2>-->
+<!--      <el-form label-width="20%">-->
+<!--        <el-form-item label="爬取问题">-->
+<!--          <el-button type="primary" id="scrapy_issue" @click="scrapy_issue">开始</el-button>&nbsp;-->
+<!--          <el-button type="success" id="scrapy_issue_detail" @click="issueTableVisible = true">查看详情</el-button>&nbsp;&nbsp;-->
+<!--          <el-text>当前状态:{{ issue_message }}</el-text>-->
+<!--        </el-form-item>-->
+<!--        <el-form-item label="爬取评论">-->
+<!--          <el-button type="primary" id="scrapy_comment" @click="scrapy_comment">开始</el-button>&nbsp;-->
+<!--          <el-button type="success" id="scrapy_comment_detail" @click="commentTableVisible = true">查看详情</el-button>&nbsp;&nbsp;-->
+<!--          <el-text>当前状态:{{ comment_message }}</el-text>-->
+<!--        </el-form-item>-->
+<!--      </el-form>-->
+<!--    </div>-->
+<!--    <div class="issueTable">-->
+<!--      <el-dialog v-model="issueTableVisible" title="问题列表">-->
+<!--        <el-table :data="issueData" id="issueDataTable">-->
+<!--          <el-table-column property="title" label="Title" width="150" show-overflow-tooltip/>-->
+<!--          <el-table-column property="body" label="Body" width="150" show-overflow-tooltip/>-->
+<!--          <el-table-column property="labels" label="Labels" width="150" show-overflow-tooltip/>-->
+<!--          <el-table-column property="created_at" label="Create_at" width="150" show-overflow-tooltip/>-->
+<!--          <el-table-column property="user" label="User" width="150" show-overflow-tooltip/>-->
+<!--        </el-table>-->
+<!--      </el-dialog>-->
+<!--    </div>-->
+<!--    <div class="commentTable">-->
+<!--      <el-dialog v-model="commentTableVisible" title="评论列表">-->
+<!--        <el-table :data="commentData" id="commentDataTable">-->
+<!--          <el-table-column property="body" label="Body" width="250" show-overflow-tooltip/>-->
+<!--          <el-table-column property="created_at" label="Create_at" width="250" show-overflow-tooltip/>-->
+<!--          <el-table-column property="user" label="User" width="250" show-overflow-tooltip/>-->
+<!--        </el-table>-->
+<!--      </el-dialog>-->
+<!--    </div>-->
+    <!--    <div>-->
+    <!--      <el-dialog title="请填写个人邮箱，以便数据爬取完成后及时通知您~" v-model="emailFormVisible">-->
+    <!--        <el-form :model="emailForm">-->
+    <!--          <el-form-item label="邮箱" prop="email" :label-width="formLabelWidth" :rules="[-->
+    <!--      { required: true, message: '请输入邮箱地址', trigger: 'blur' },-->
+    <!--      { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }]">-->
+    <!--            <el-input v-model="emailForm.email" autocomplete="off"></el-input>-->
+    <!--          </el-form-item>-->
+    <!--        </el-form>-->
+    <!--        <template #footer>-->
+    <!--    <span class="dialog-footer">-->
+    <!--      <el-button @click="emailFormVisible = false">取 消</el-button>-->
+    <!--      <el-button type="primary" @click="submitEmailForm">确 定</el-button>-->
+    <!--    </span>-->
+    <!--        </template>-->
+    <!--      </el-dialog>-->
+    <!--    </div>-->
+<!--    <div class="step3">-->
+<!--      <h2>数据下载</h2>-->
+<!--      <el-form label-width="20%">-->
+<!--        <el-form-item label="下载格式">-->
+<!--          <el-radio v-model="radio" label="csv">csv</el-radio>-->
+<!--          <el-radio v-model="radio" label="xlsx">xlsx</el-radio>-->
+<!--          <el-radio v-model="radio" label="txt">txt</el-radio>-->
+<!--          <el-button type="primary" id="download_issue" @click="download('issues',this.radio)">下载问题</el-button>-->
+<!--          <el-button type="primary" id="download_comment" @click="download('comments',this.radio)">下载评论</el-button>-->
+<!--        </el-form-item>-->
+<!--      </el-form>-->
+<!--    </div>-->
   </div>
 </template>
 
@@ -119,10 +112,10 @@ export default {
       issueTableVisible: false,
       commentTableVisible: false,
       form: {
-        name: "https://github.com/apache/superset",
-        version: '',
-        startTime: '',
-        endTime: '',
+        repo: "apache/superset",
+        since: '2023-5-1',
+        until: Date.now(),
+        email: '3320415065@qq.com'
       },
       radio: 'csv',
       issue_message: "未处理",
@@ -144,11 +137,12 @@ export default {
       });
     },
     onSubmit() {
-      if (this.form.name !== '') {
+      if (this.form.repo !== '') {
         this.$message({
           message: '数据源保存成功！',
           type: 'success'
         });
+        this.repo_name = this.form.repo
         this.comment_message = "未处理";
         this.issue_message = "未处理";
       } else {
@@ -156,14 +150,20 @@ export default {
           message: '项目链接不能为空！',
           type: 'error'
         });
-        return
       }
+    },
+    crawling() {
+      this.form.since = this.dateFormat(this.form.since)
+      this.form.until = this.dateFormat(this.form.until)
       axios.post(
-          "/api/project/info",
+          "api/crawling",
           this.form
       ).then((res) => {
-        this.repo_name = res.data
+        console.log(res)
       })
+    },
+    dateFormat(date) {
+      return date.getFullYear() + '-' + date.getMonth() + '-' + date.getDay();
     },
     checkRepoNotNull() {
       return !(this.repo_name === "" || this.repo_name === "未定义");
