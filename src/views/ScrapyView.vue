a03f233a72dedb5cc5b984c99f73cf0403a9eca1
Yueming <201830115@smail.nju.edu.cn>
2023-05-27 22:35:53 +0800

    finish:重构前端爬取+下载

--- src/views/ScrapyView.vue
+++ src/views/ScrapyView.vue
@@ -24,76 +24,38 @@
           <el-input v-model="form.email" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item>
+          <el-col class="line" :span="11">当前项目:{{ repo_name }}</el-col>&nbsp;
+          <el-col class="line" :span="11">当前状态:{{ scrapy_status }}</el-col>
+        </el-form-item>
+        <el-form-item>
           <el-button type="primary" @click="onSubmit">保存</el-button>&nbsp;
           <el-button type="primary" @click="crawling">爬取</el-button>&nbsp;&nbsp;
-          <el-text>当前项目:{{ repo_name }}</el-text>
+          <el-button type="success" id="scrapy_issue_detail" @click="dataTableVisible = true">查看详情</el-button>
         </el-form-item>
       </el-form>
     </div>
-    <!--    <div class="step2">-->
-    <!--      <h2>数据获取</h2>-->
-    <!--      <el-form label-width="20%">-->
-    <!--        <el-form-item label="爬取问题">-->
-    <!--          <el-button type="primary" id="scrapy_issue" @click="scrapy_issue">开始</el-button>&nbsp;-->
-    <!--          <el-button type="success" id="scrapy_issue_detail" @click="issueTableVisible = true">查看详情</el-button>&nbsp;&nbsp;-->
-    <!--          <el-text>当前状态:{{ issue_message }}</el-text>-->
-    <!--        </el-form-item>-->
-    <!--        <el-form-item label="爬取评论">-->
-    <!--          <el-button type="primary" id="scrapy_comment" @click="scrapy_comment">开始</el-button>&nbsp;-->
-    <!--          <el-button type="success" id="scrapy_comment_detail" @click="commentTableVisible = true">查看详情</el-button>&nbsp;&nbsp;-->
-    <!--          <el-text>当前状态:{{ comment_message }}</el-text>-->
-    <!--        </el-form-item>-->
-    <!--      </el-form>-->
-    <!--    </div>-->
-    <!--    <div class="issueTable">-->
-    <!--      <el-dialog v-model="issueTableVisible" title="问题列表">-->
-    <!--        <el-table :data="issueData" id="issueDataTable">-->
-    <!--          <el-table-column property="title" label="Title" width="150" show-overflow-tooltip/>-->
-    <!--          <el-table-column property="body" label="Body" width="150" show-overflow-tooltip/>-->
-    <!--          <el-table-column property="labels" label="Labels" width="150" show-overflow-tooltip/>-->
-    <!--          <el-table-column property="created_at" label="Create_at" width="150" show-overflow-tooltip/>-->
-    <!--          <el-table-column property="user" label="User" width="150" show-overflow-tooltip/>-->
-    <!--        </el-table>-->
-    <!--      </el-dialog>-->
-    <!--    </div>-->
-    <!--    <div class="commentTable">-->
-    <!--      <el-dialog v-model="commentTableVisible" title="评论列表">-->
-    <!--        <el-table :data="commentData" id="commentDataTable">-->
-    <!--          <el-table-column property="body" label="Body" width="250" show-overflow-tooltip/>-->
-    <!--          <el-table-column property="created_at" label="Create_at" width="250" show-overflow-tooltip/>-->
-    <!--          <el-table-column property="user" label="User" width="250" show-overflow-tooltip/>-->
-    <!--        </el-table>-->
-    <!--      </el-dialog>-->
-    <!--    </div>-->
-    <!--    <div>-->
-    <!--      <el-dialog title="请填写个人邮箱，以便数据爬取完成后及时通知您~" v-model="emailFormVisible">-->
-    <!--        <el-form :model="emailForm">-->
-    <!--          <el-form-item label="邮箱" prop="email" :label-width="formLabelWidth" :rules="[-->
-    <!--      { required: true, message: '请输入邮箱地址', trigger: 'blur' },-->
-    <!--      { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }]">-->
-    <!--            <el-input v-model="emailForm.email" autocomplete="off"></el-input>-->
-    <!--          </el-form-item>-->
-    <!--        </el-form>-->
-    <!--        <template #footer>-->
-    <!--    <span class="dialog-footer">-->
-    <!--      <el-button @click="emailFormVisible = false">取 消</el-button>-->
-    <!--      <el-button type="primary" @click="submitEmailForm">确 定</el-button>-->
-    <!--    </span>-->
-    <!--        </template>-->
-    <!--      </el-dialog>-->
-    <!--    </div>-->
-    <!--    <div class="step3">-->
-    <!--      <h2>数据下载</h2>-->
-    <!--      <el-form label-width="20%">-->
-    <!--        <el-form-item label="下载格式">-->
-    <!--          <el-radio v-model="radio" label="csv">csv</el-radio>-->
-    <!--          <el-radio v-model="radio" label="xlsx">xlsx</el-radio>-->
-    <!--          <el-radio v-model="radio" label="txt">txt</el-radio>-->
-    <!--          <el-button type="primary" id="download_issue" @click="download('issues',this.radio)">下载问题</el-button>-->
-    <!--          <el-button type="primary" id="download_comment" @click="download('comments',this.radio)">下载评论</el-button>-->
-    <!--        </el-form-item>-->
-    <!--      </el-form>-->
-    <!--    </div>-->
+    <div class="dataTable">
+      <el-dialog v-model="dataTableVisible" title="问题列表">
+        <el-table :data="issueData" id="issueDataTable">
+          <el-table-column property="title" label="Title" width="150" show-overflow-tooltip/>
+          <el-table-column property="body" label="Body" width="150" show-overflow-tooltip/>
+          <el-table-column property="labels" label="Labels" width="150" show-overflow-tooltip/>
+          <el-table-column property="created_at" label="Create_at" width="150" show-overflow-tooltip/>
+          <el-table-column property="user" label="User" width="150" show-overflow-tooltip/>
+        </el-table>
+      </el-dialog>
+    </div>
+    <div class="step3">
+      <h2>数据下载</h2>
+      <el-form label-width="20%">
+        <el-form-item label="下载格式">
+          <el-radio v-model="radio" label="csv">csv</el-radio>
+          <el-radio v-model="radio" label="xlsx">xlsx</el-radio>
+          <el-radio v-model="radio" label="txt">txt</el-radio>
+          <el-button type="primary" id="download" @click="download(this.radio)">下载</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -108,150 +70,85 @@ export default {
   data() {
     return {
       issueData: [],
-      commentData: [],
-      issueTableVisible: false,
-      commentTableVisible: false,
+      dataTableVisible: false,
       form: {
         repo: "apache/superset",
         since: Date.now(),
         until: Date.now(),
         email: '3320415065@qq.com'
       },
+      scrapy_status: '未处理',
       radio: 'csv',
-      issue_message: "未处理",
-      comment_message: "未处理",
       repo_name: "未定义",
-      emailFormVisible: false,
-      emailForm: {
-        email: '',
-      },
-      formLabelWidth: '120px'
     }
   },
   methods: {
-    submitEmailForm() {
-      this.emailFormVisible = false;
-      this.$message({
-        message: '邮箱提交成功！',
-        type: 'success'
-      });
-    },
     onSubmit() {
-      if (this.form.repo !== '') {
+      if (this.form.repo === '' || this.form.email === '') {
         this.$message({
-          message: '数据源保存成功！',
-          type: 'success'
-        });
-        this.repo_name = this.form.repo
-        this.comment_message = "未处理";
-        this.issue_message = "未处理";
-      } else {
-        this.$message({
-          message: '项目链接不能为空！',
+          message: '项目配置不完整！',
           type: 'error'
         });
+        return
+      }
+      this.$message({
+        message: '数据源保存成功！',
+        type: 'success'
+      });
+      this.repo_name = this.form.repo;
+      this.scrapy_status = '未处理';
+    },
+    checkRepoNull() {
+      if (this.repo_name === "未定义" || this.repo_name === '') {
+        this.$message({
+          message: '请检查配置是否保存',
+          type: 'warning'
+        });
+        return true
+      }
+    },
+    checkRepeatTry() {
+      if (this.scrapy_status !== '未处理') {
+        this.$message({
+          message: '数据已爬取，请不要重试！',
+          type: 'warning'
+        });
+        return true
       }
     },
     crawling() {
+      if (this.checkRepoNull() || this.checkRepeatTry()) {
+        return
+      }
+      this.scrapy_status = '数据爬取中，请稍候……';
       this.form.since = this.dateFormat(this.form.since)
       this.form.until = this.dateFormat(this.form.until)
       axios.post(
           "api/crawling",
           this.form
       ).then((res) => {
-        console.log(res)
+        res.data.forEach(item => {
+          let labelsName = "";
+          item.labels.forEach(_item => {
+            labelsName += _item.name + " ";
+          })
+          item.labels = labelsName
+          item.user = item.user.login
+          this.issueData = this.issueData.concat(item)
+        })
+        this.$message({
+          message: '爬取完成！',
+          type: 'success'
+        });
+        this.scrapy_status = '已完成！'
       })
     },
     dateFormat(date) {
       const this_date = new Date(date);
       return this_date.getFullYear() + '-' + (this_date.getMonth() + 1) + '-' + this_date.getDate();
     },
-    checkRepoNotNull() {
-      return !(this.repo_name === "" || this.repo_name === "未定义");
-    },
-    repoNullError() {
-      this.$message({
-        message: '项目名称不能为空！',
-        type: 'error'
-      });
-    },
-    RepeatTryError() {
-      this.$message({
-        message: '数据已爬取，请不要重试！',
-        type: 'error'
-      });
-    },
-    scrapy_issue() {
-      if (this.checkRepoNotNull()) {
-        if (this.issue_message === "未处理") {
-          if (this.emailForm.email === '') {
-            this.emailFormVisible = true;
-          }
-          this.issue_message = "正在爬取问题，请稍候……";
-          this.$message({
-            message: this.issue_message,
-            type: 'success'
-          });
-          axios.get("/api/issue/get-and-save-db").then((res) => {
-            this.issue_message = "爬取成功！";
-            res.data.forEach(item => {
-              let labelsName = "";
-              item.labels.forEach(_item => {
-                labelsName += _item.name + " ";
-              })
-              item.labels = labelsName
-              item.user = item.user.login
-              this.issueData = this.issueData.concat(item)
-            })
-            this.$message({
-              message: this.issue_message,
-              type: 'success'
-            });
-            axios.post(
-                "/api/email",
-                this.emailForm
-            )
-          });
-        } else {
-          this.RepeatTryError()
-        }
-      } else {
-        this.repoNullError()
-      }
-    },
-    scrapy_comment() {
-      if (this.checkRepoNotNull()) {
-        if (this.comment_message === "未处理") {
-          console.log(this.emailForm.email)
-          if (this.emailForm.email === '') {
-            this.emailFormVisible = true;
-          }
-          this.comment_message = "正在爬取评论，请稍候……";
-          this.$message({
-            message: this.comment_message,
-            type: 'success'
-          });
-          axios.get("/api/issue/comments/get-and-save-db").then((res) => {
-            this.comment_message = "爬取成功！";
-            res.data.forEach(item => {
-              console.log(item)
-              item.user = item.user.login
-              this.commentData = this.commentData.concat(item)
-            })
-            this.$message({
-              message: this.comment_message,
-              type: 'success'
-            });
-          });
-        } else {
-          this.RepeatTryError()
-        }
-      } else {
-        this.repoNullError()
-      }
-    },
-    generateData(type, format) {
-      const data = type === 'issues' ? this.issueData : this.commentData;
+    generateData(format) {
+      const data = this.issueData;
       if (format === 'csv') {
         const csvData = Papa.unparse(data);
         return {data: csvData, extension: 'csv'};
@@ -279,30 +176,23 @@ export default {
         return {data: xlsxData, extension: 'xlsx'};
       } else if (format === 'txt') {
         let txtData = '';
-        if (type === "issues") {
-          txtData += "Title\tBody\tLabels\tCreated_at\tUser\n";
-          data.forEach(item => {
-            txtData += `${item.title}\t${item.body}\t${item.labels}\t${item.created_at}\t${item.user}\n`;
-          });
-        } else if (type === "comments") {
-          txtData += "Body\tCreated_at\tUser\n";
-          data.forEach(item => {
-            txtData += `${item.body}\t${item.created_at}\t${item.user}\n`;
-          });
-        }
+        txtData += "Title\tBody\tLabels\tCreated_at\tUser\n";
+        data.forEach(item => {
+          txtData += `${item.title}\t${item.body}\t${item.labels}\t${item.created_at}\t${item.user}\n`;
+        });
         return {data: txtData, extension: 'txt'};
       } else {
         return null; // 处理未知格式
       }
     },
-    download(type, format) {
-      const formatData = this.generateData(type, format);
+    download(format) {
+      const formatData = this.generateData(format);
       if (formatData) {
         const {data, extension} = formatData;
         const blob = new Blob([data], {type: `text/${extension};charset=utf-8;`});
         const downloadLink = document.createElement('a');
         downloadLink.href = URL.createObjectURL(blob);
-        downloadLink.download = `${type}.${extension}`;
+        downloadLink.download = `issues.${extension}`;
         document.body.appendChild(downloadLink);
         downloadLink.click();
         document.body.removeChild(downloadLink);
